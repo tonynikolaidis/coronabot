8353a86ca81d65237556744fca1d9bb4e4cd02b1
anthonynikolaidis <anthony.nikolaidis@infopelago.gr>
2020-05-17 17:23:23 +0300

    adding required imports

---
+++ imports.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo /opt/python38/bin/pip install discord
+sudo /opt/python38/bin/pip install asyncio
+sudo /opt/python38/bin/pip install requests
+sudo /opt/python38/bin/pip install Pillow
+sudo /opt/python38/bin/pip install urllib3
+sudo /opt/python38/bin/pip install matplotlib
+sudo /opt/python38/bin/pip install millify
